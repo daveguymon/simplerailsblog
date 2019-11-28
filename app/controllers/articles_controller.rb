39c543bf56ec29e1ed8843d44b6809dff05c680e
Dave Guymon <davidwguymon@gmail.com>
2019-11-27 22:03:18 -0700

    Completed MVP

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
 
-  http_basic_authenticate_with name: "harper", password: "boogerald", except: [:index, :show]
+  http_basic_authenticate_with name: "harper", password: "iloveboo", except: [:index, :show]
 
   def index
     @articles = Article.all
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
 
-  http_basic_authenticate_with name: "harper", password: "boogerald", only: :destroy
+  http_basic_authenticate_with name: "harper", password: "iloveboo", only: :destroy
 
   def create
     @article = Article.find(params[:article_id])
