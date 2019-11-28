52a591a32ac08758a84474ee849eea493910eafc
Dave Guymon <davidwguymon@gmail.com>
2019-11-27 23:57:18 -0700

    refactored variables into yml file

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
 
-  http_basic_authenticate_with name: "harper", password: "iloveboo", only: :destroy
+  http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"], only: :destroy
 
   def create
     @article = Article.find(params[:article_id])
